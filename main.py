556b29a29d6a62c944c717efeba548316519edbd
Mikołaj Mroczkowski <mikolaj.mroczkowski@gmail.com>
2021-12-08 13:37:41 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def Sito(n):
+    tab = list()
+    for x in range(0,n):
+        tab.append("T")
+    tab[0]="F"
+    tab[1]="F"
+    for obj in range(2,int(n**0.5)+1):
+        if tab[obj] == "T":
+            for x in range(obj*obj,n,obj):
+                tab[x] = "F"
+    wyniki =""
+    for x in range (len(tab)):
+        if tab[x]=="T":
+            wyniki += str(x)+" "
+    return wyniki
+print(Sito(10))
+
